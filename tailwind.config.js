0cd6dac2f2b387abbfaffd996a6f392efbc5c03b
Eunji Gwak <cpzmalsus319@gmail.com>
2025-08-29 17:57:39 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // src 폴더의 모든 파일을 포함
+    "./public/index.html", // 필요하면 public 폴더의 HTML도 포함
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans KR", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
